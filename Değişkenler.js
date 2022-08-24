377c454f61af76ba422a806fa47698d5a8129d38
Mehmet <98388377+mehmethayirli@users.noreply.github.com>
2022-08-24 18:42:44 +0300

    Add files via upload

---
+++ Değişkenler.js
@@ -0,0 +1,16 @@
+var isim = "Mehmet";
+var soyad = "Hayirli";
+var dogumTarihi = "2002";
+var not = 75;
+var not2 = 57;
+var not3 = 83;
+var ort = (not+not2+not3)/3;
+
+//var guncelYil = new Date().getFullYear();
+//var yas = guncelYil - pareseInt(dogumTarihi);
+//console.log(yas);
+
+console.log(ort); // tamsayi icin = console.log(parseInt(ort));
+console.log(ort.toFixed(2)); // virgulden sonraki basamak sayisi
+console.log(typeof ort); //degisken turu
+console.log(ort>50);
