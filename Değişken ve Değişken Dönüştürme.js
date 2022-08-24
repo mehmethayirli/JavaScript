d96dea575c2c6dc3cd6f423cfa2a1322b0b14df0
Mehmet <98388377+mehmethayirli@users.noreply.github.com>
2022-08-24 18:42:10 +0300

    Add files via upload

---
+++ Değişken ve Değişken Dönüştürme.js
@@ -0,0 +1,19 @@
+var isim = "Mehmet";
+var soyad = "Hayirli";
+var dogumTarihi = 2002;
+
+console.log(isim);
+console.log(typeof isim);
+console.log(typeof dogumTarihi);
+
+let sayi1= "10";
+let sayi2= "20";
+
+console.log(sayi1+sayi2); //cikti-> 1020
+console.log(Number(sayi1) + Number(sayi2)); //string->number degisken donusturme
+
+let sayi3= 10;
+let sayi4= 20;
+
+console.log(sayi3+sayi4); //cikti-> 30
+console.log(sayi3.toString() + sayi4.toString());
